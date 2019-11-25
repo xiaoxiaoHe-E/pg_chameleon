822b932e55483dc24e30a4ee40a930a4dafdb16c
xiaoxiaoHe-E <53000479+xiaoxiaoHe-E@users.noreply.github.com>
2019-11-25 17:55:19 +0800

    Update __init__.py

--- pg_chameleon/__init__.py
+++ pg_chameleon/__init__.py
@@ -3,4 +3,7 @@ from .lib.mysql_lib import *
 from .lib.pg_lib import *
 from .lib.global_lib import *
 
-from .gpss import *
+from .gpss.data_pb2_grpc import *
+from .gpss.data_pb2 import *
+from .gpss.gpss_pb2_grpc import *
+from .gpss.gpss_pb2 import *
